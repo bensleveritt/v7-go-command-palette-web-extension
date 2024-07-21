0ba7390cedc9365feb40a063391be64b45e36ed7
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-07-21 23:08:09 +0100

    Add escape key test

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,43 @@
 import { expect, test } from "./fixtures";
 
-test("launches command palette", async ({ page }) => {
+test("launches and closes command palette", async ({ page }) => {
+  await page.goto("https://go.v7labs.com");
+
+  // Fill in username and password
+  await page.fill("input#username", process.env.TEST_EMAIL);
+  await page.fill("input#password", process.env.TEST_PASSWORD);
+
+  // Submit the form
+  await page.click('button[type="submit"]');
+
+  // Wait for the page to load
+  await page.waitForURL("https://go.v7labs.com/**");
+  await page.waitForSelector('role=link[name*="Recent projects"]');
+
+  await expect(
+    page.getByRole("dialog", { name: /Command Palette/ })
+  ).not.toBeVisible();
+
+  // Press cmd+k
+  await page.keyboard.down("ControlOrMeta");
+  await page.keyboard.press("k");
+  await page.keyboard.up("ControlOrMeta");
+
+  await expect(
+    page.getByRole("dialog", { name: /Command Palette/ })
+  ).toBeVisible();
+
+  // Press cmd+k again
+  await page.keyboard.down("ControlOrMeta");
+  await page.keyboard.press("k");
+  await page.keyboard.up("ControlOrMeta");
+
+  await expect(
+    page.getByRole("dialog", { name: /Command Palette/ })
+  ).not.toBeVisible();
+});
+
+test("escape closes the command palette", async ({ page }) => {
   await page.goto("https://go.v7labs.com");
 
   // Fill in username and password
@@ -22,9 +59,11 @@ test("launches command palette", async ({ page }) => {
   await expect(
     page.getByRole("dialog", { name: /Command Palette/ })
   ).toBeVisible();
-});
 
-// test('popup page', async ({ page, extensionId }) => {
-//   await page.goto(`chrome-extension://${extensionId}/popup.html`);
-//   await expect(page.locator('body')).toHaveText('my-extension popup');
-// });
+  // Press cmd+k again
+  await page.keyboard.press("Escape");
+
+  await expect(
+    page.getByRole("dialog", { name: /Command Palette/ })
+  ).not.toBeVisible();
+});
