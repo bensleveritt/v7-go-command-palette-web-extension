f3c95d01b639b1c421ee525244b4cf8ff217d6ac
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-07-21 08:33:41 +0100

    Add basic dialog wrapper style

--- src/pages/CommandPalette.svelte
+++ src/pages/CommandPalette.svelte
@@ -1,18 +1,14 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
+  import { fade, fly } from "svelte/transition";
 
   const open = writable(false);
-  let dialogEl: HTMLDialogElement;
 
   onMount(() => {
     document.addEventListener("keydown", (e) => {
       if (e.key === "k" && e.metaKey) {
-        if (dialogEl.open) {
-          dialogEl.close();
-        } else {
-          dialogEl.showModal();
-        }
+        open.set(!$open);
       }
     });
   });
@@ -26,25 +22,48 @@
   ];
 </script>
 
-<dialog bind:this={dialogEl} class="shadow-lg">
-  <header>
-    <input type="text" />
-    <button on:click={() => open.set(false)}>Close</button>
-  </header>
+{#if $open}
+  <div
+    class="dialog-wrapper fixed inset-0 backdrop-blur-xl"
+    aria-modal="true"
+    role="dialog"
+    aria-labelledby="dialogTitle"
+    transition:fade={{ duration: 200 }}
+  >
+    <dialog class="shadow-lg" open transition:fly={{ y: 20, duration: 200 }}>
+      <header>
+        <h2 id="dialogTitle">Command Palette</h2>
+        <input type="text" />
+        <button on:click={() => open.set(false)}>Close</button>
+      </header>
 
-  <ul>
-    {#each commands as command}
-      <li><button on:click={() => console.log(command)}>{command}</button></li>
-    {/each}
-  </ul>
-</dialog>
+      <ul>
+        {#each commands as command}
+          <li>
+            <button on:click={() => console.log(command)}>{command}</button>
+          </li>
+        {/each}
+      </ul>
+    </dialog>
+  </div>
+{/if}
 
 <style>
+  .dialog-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.06);
+    backdrop-filter: blur(4px);
+    z-index: 2147483001; /* 1 more than intercom */
+  }
+
   dialog {
     color: var(--color-text-default);
     background: var(--color-surface-default);
     border-radius: 8px;
     margin: auto;
     padding: 1rem;
+    position: fixed;
   }
 </style>
